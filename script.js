cf2ccbe829e7c4eeb5c93535358fe2992e4d00e6
Jack Phalen <jack.phalen@gmail.com>
2023-07-22 11:57:02 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const video = document.getElementById('video');
+const button = document.getElementById('button');
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia(); 
+        video.srcObject = mediaStream;
+        video.onloadedmetadata = () => {
+            video.play();
+        }
+    }
+
+    catch (err) {
+    }
+}
+
+button.addEventListener('click', async () => {
+    button.disabled = true;
+
+    await video.requestPictureInPicture();
+    button.disabled = false;
+
+});
+
+selectMediaStream();
